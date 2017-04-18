f2ed99b05ec0a2c4de4ea1f7a372bc1fd80083c2
Stan Chlebicki <5tan@users.noreply.github.com>
2017-04-18 17:10:17 +0200

    added test project

---
+++ tests/main.cpp
@@ -0,0 +1,45 @@
+#include <cxxmidi/file.hpp>
+#include <cxxmidi/output/default.hpp>
+#include <cxxmidi/player/synchronous.hpp>
+#include <cxxmidi/callback.hpp>
+
+class PlayerHeartbeatCallback : public CxxMidi::Callback<CxxMidi::Player::Abstract>
+{
+public:
+    PlayerHeartbeatCallback(){}
+
+    virtual void operator()(CxxMidi::Player::Abstract* player_)
+    {
+        std::cerr << player_->currentTimePos() << std::endl;
+    }
+};
+
+class PlayerFinishedCallback : public CxxMidi::Callback<CxxMidi::Player::Abstract>
+{
+public:
+    PlayerFinishedCallback(){}
+
+    virtual void operator()(CxxMidi::Player::Abstract*)
+    {
+        std::cerr << "done!" << std::endl;
+    }
+
+};
+
+int main(int /*argc*/, char ** /*argv*/)
+{
+    CxxMidi::Output::Default output(0);
+    CxxMidi::Player::Synchronous player(&output);
+
+    CxxMidi::File file("C:\\sample.mid");
+    std::cerr << file.tracks() << std::endl;
+    player.setFile(&file);
+
+    PlayerHeartbeatCallback playerHeartbeatCallback;
+    player.setCallbackHeartbeat(&playerHeartbeatCallback);
+
+    PlayerFinishedCallback playerFinishedCallback;
+    player.setCallbackFinished(&playerFinishedCallback);
+
+    player.play();
+}
