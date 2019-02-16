4f9412c032ad7274cf73c798ffee14ca6501834d
Stan Chlebicki <5tan@users.noreply.github.com>
2019-02-16 21:47:30 +0100

    Fixed some var names

--- include/cxxmidi/event.hpp
+++ include/cxxmidi/event.hpp
@@ -10,14 +10,14 @@ namespace cxxmidi {
 class Event : public Message {
  public:
   inline Event();
-  inline Event(uint32_t dt_);
-  inline Event(uint32_t dt_, const Message &message_);
-  inline Event(uint32_t dt_, uint8_t b1_);
-  inline Event(uint32_t dt_, uint8_t b1_, uint8_t b2_);
-  inline Event(uint32_t dt_, uint8_t b1_, uint8_t b2_, uint8_t b3_);
+  inline Event(uint32_t dt);
+  inline Event(uint32_t dt, const Message &message);
+  inline Event(uint32_t dt, uint8_t b1);
+  inline Event(uint32_t dt, uint8_t b1, uint8_t b2);
+  inline Event(uint32_t dt, uint8_t b1, uint8_t b2, uint8_t b3);
 
   inline uint32_t Dt() const;
-  inline void SetDt(uint32_t dt_);
+  inline void SetDt(uint32_t dt);
 
  private:
   uint32_t _dt;  // delta time [ticks]
@@ -29,29 +29,29 @@ namespace cxxmidi {
 
 Event::Event() {}
 
-Event::Event(uint32_t dt_) : _dt(dt_) {}
+Event::Event(uint32_t dt) : _dt(dt) {}
 
-Event::Event(uint32_t dt_, const Message &message_)
-    : Message(message_), _dt(dt_) {}
+Event::Event(uint32_t dt, const Message &message)
+    : Message(message), _dt(dt) {}
 
-Event::Event(uint32_t dt_, uint8_t b1_) : _dt(dt_) { this->push_back(b1_); }
+Event::Event(uint32_t dt, uint8_t b1) : _dt(dt) { this->push_back(b1); }
 
-Event::Event(uint32_t dt_, uint8_t b1_, uint8_t b2_) : _dt(dt_) {
+Event::Event(uint32_t dt, uint8_t b1, uint8_t b2) : _dt(dt) {
   this->reserve(2);
-  this->push_back(b1_);
-  this->push_back(b2_);
+  this->push_back(b1);
+  this->push_back(b2);
 }
 
-Event::Event(uint32_t dt_, uint8_t b1_, uint8_t b2_, uint8_t b3_) : _dt(dt_) {
+Event::Event(uint32_t dt, uint8_t b1, uint8_t b2, uint8_t b3) : _dt(dt) {
   this->reserve(3);
-  this->push_back(b1_);
-  this->push_back(b2_);
-  this->push_back(b3_);
+  this->push_back(b1);
+  this->push_back(b2);
+  this->push_back(b3);
 }
 
 uint32_t Event::Dt() const { return _dt; }
 
-void Event::SetDt(uint32_t dt_) { _dt = dt_; }
+void Event::SetDt(uint32_t dt) { _dt = dt; }
 
 }  // namespace CxxMidi
 
