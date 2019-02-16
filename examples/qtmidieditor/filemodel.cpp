3b769920ec49b9224c957558f108d590062baa9f
Stan <5tan@users.noreply.github.com>
2019-02-16 22:22:25 +0100

    Fixed some var names

--- examples/qtmidieditor/filemodel.cpp
+++ examples/qtmidieditor/filemodel.cpp
@@ -1,39 +1,39 @@
 #include "filemodel.h"
 
-FileModel::FileModel(QObject *parent_) : QAbstractListModel(parent_) {}
+FileModel::FileModel(QObject *parent) : QAbstractListModel(parent) {}
 
 FileModel::~FileModel() {}
 
-int FileModel::rowCount(const QModelIndex & /* index_ */) const {
+int FileModel::rowCount(const QModelIndex & /* index */) const {
   if (_file) return static_cast<int>(_file->Tracks());
 
   return 0;
 }
 
-void FileModel::SetFile(cxxmidi::File *file_) {
-  _file = file_;
+void FileModel::SetFile(cxxmidi::File *file) {
+  _file = file;
   this->layoutChanged();
 }
 
-void FileModel::RemoveTrack(int num_) {
-  this->beginRemoveRows(QModelIndex(), num_, num_);
-  _file->erase(_file->begin() + num_);
+void FileModel::RemoveTrack(int num) {
+  this->beginRemoveRows(QModelIndex(), num, num);
+  _file->erase(_file->begin() + num);
   this->endRemoveRows();
 }
 
-void FileModel::AddTrack(int num_) {
-  this->beginInsertRows(QModelIndex(), num_, num_);
-  cxxmidi::File::iterator it = _file->begin() + num_;
+void FileModel::AddTrack(int num) {
+  this->beginInsertRows(QModelIndex(), num, num);
+  cxxmidi::File::iterator it = _file->begin() + num;
   _file->insert(it, cxxmidi::Track());
   this->endInsertRows();
 }
 
-QVariant FileModel::data(const QModelIndex &index_, int role_) const {
-  if (!index_.isValid()) return QVariant();
+QVariant FileModel::data(const QModelIndex &index, int role) const {
+  if (!index.isValid()) return QVariant();
 
-  int row = index_.row();
+  int row = index.row();
 
-  if (role_ == Qt::DisplayRole) {
+  if (role == Qt::DisplayRole) {
     cxxmidi::Track *track = &_file->at(row);
     return QVariant(QString("%1. %2").arg(row).arg(track->GetName().c_str()));
   }
