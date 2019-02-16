46f0c543e9b88bc75eeb793e99b53a331149d0f9
Stan <5tan@users.noreply.github.com>
2019-02-16 21:28:38 +0100

    Fixed some var and fn names

--- tests/instrument.cpp
+++ tests/instrument.cpp
@@ -2,8 +2,8 @@
 #include <cxxmidi/instrument.hpp>
 
 TEST(Instrument, GetSomeNames) {
-  EXPECT_STREQ(cxxmidi::Instrument::Name(0).c_str(), "Acoustic Grand Piano");
-  EXPECT_STREQ(cxxmidi::Instrument::Name(127).c_str(), "Gunshot");
+  EXPECT_STREQ(cxxmidi::Instrument::GetName(0).c_str(), "Acoustic Grand Piano");
+  EXPECT_STREQ(cxxmidi::Instrument::GetName(127).c_str(), "Gunshot");
 }
 
 TEST(Instrument, GetSomeIds) {
