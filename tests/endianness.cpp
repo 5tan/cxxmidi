ce236983fdcdebeed3e6f84bea5d3fa315bc7f68
Stan Chlebicki <5tan@users.noreply.github.com>
2019-02-18 15:38:47 +0100

    Added endianness test

---
+++ tests/endianness.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include <cxxmidi/guts/endianness.hpp>
+
+TEST(Endianness, Fix) {
+  std::ifstream ifs("../../cxxmidi/music/chopin.mid");
+  uint32_t data;
+  ifs.read(reinterpret_cast<char *>(&data), 4);
+
+  if (cxxmidi::guts::endianness::MachineIsLittleEndian()) {
+      ASSERT_EQ(data, 0x6468544d); // "dhTM" (fix needed)
+      ASSERT_EQ(cxxmidi::guts::endianness::Fix(data), 0x4d546864); // "MThd"
+  }
+  else {
+      ASSERT_EQ(data, 0x4d546864); // "MThd" (no fix needed)
+      ASSERT_EQ(cxxmidi::guts::endianness::Fix(data), 0x6468544d); // "dhTM"
+  }
+}
