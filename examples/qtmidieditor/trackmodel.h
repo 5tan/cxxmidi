90d4302cd1d8d62055a880072a1874a79d6a5067
Stan <5tan@users.noreply.github.com>
2019-02-19 19:08:53 +0100

    Style corrections

--- examples/qtmidieditor/trackmodel.h
+++ examples/qtmidieditor/trackmodel.h
@@ -1,5 +1,27 @@
-#ifndef TRACKMODEL_H
-#define TRACKMODEL_H
+/* *****************************************************************************
+Copyright (c) 2018 5tan 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+***************************************************************************** */
+
+#ifndef EXAMPLES_QTMIDIEDITOR_TRACKMODEL_H_
+#define EXAMPLES_QTMIDIEDITOR_TRACKMODEL_H_
 
 #include <QAbstractTableModel>
 #include <QObject>
@@ -20,9 +42,8 @@ class TrackModel : public QAbstractTableModel {
   static const char* columnNames[];
 
  public:
-  TrackModel(QObject* parent = 0);
-
-  virtual ~TrackModel();
+  explicit TrackModel(QObject* parent = 0);
+  virtual ~TrackModel() = default;
 
   Qt::ItemFlags flags(const QModelIndex& /*index*/) const;
   virtual int rowCount(const QModelIndex& index) const;
@@ -43,4 +64,4 @@ class TrackModel : public QAbstractTableModel {
   cxxmidi::Track* track_;
 };
 
-#endif  // TRACKMODEL_H
+#endif  // EXAMPLES_QTMIDIEDITOR_TRACKMODEL_H_
