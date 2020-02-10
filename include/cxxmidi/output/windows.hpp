9d6205e89598937a6b1eb17e607db24dca514ac2
Stan Chlebicki <5tan@users.noreply.github.com>
2020-02-10 23:34:34 +0100

    Using override

--- include/cxxmidi/output/windows.hpp
+++ include/cxxmidi/output/windows.hpp
@@ -64,16 +64,16 @@ class Windows : public Output::Abstract {
   Windows(const Windows &);             // non-copyable
   Windows &operator=(const Windows &);  // non-copyable (assignment)
 
-  inline virtual void OpenPort(unsigned int portNumber_ = 0);
-  inline virtual void ClosePort();
+  inline virtual void OpenPort(unsigned int portNumber_ = 0) override;
+  inline virtual void ClosePort() override;
   inline virtual void OpenVirtualPort(
       const std::string &portName_ = std::string("CxxMidi Output"));
-  inline virtual size_t GetPortCount();
-  inline virtual std::string GetPortName(unsigned int portNumber_ = 0);
-  inline virtual void SendMessage(const std::vector<uint8_t> *msg_);
+  inline virtual size_t GetPortCount() override;
+  inline virtual std::string GetPortName(unsigned int portNumber_ = 0) override;
+  inline virtual void SendMessage(const std::vector<uint8_t> *msg_) override;
 
  protected:
-  inline virtual void Initialize();
+  inline virtual void Initialize() override;
 
  private:
   void *api_data_;
