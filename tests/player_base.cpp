b367b6da8538e9ce42693d3662ddd88a17a31fd3
Stan <5tan@users.noreply.github.com>
2020-02-11 23:42:14 +0100

    Added missing file

---
+++ tests/player_base.cpp
@@ -0,0 +1,37 @@
+/* *****************************************************************************
+Copyright (c) 2018 5tan 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+***************************************************************************** */
+
+#include <gtest/gtest.h>
+
+#include <cxxmidi/guts/player_base.hpp>
+
+TEST(PlayerBase, InitialState) { 
+    cxxmidi::guts::PlayerBase player_base;
+    ASSERT_EQ(player_base.CurrentTimePos(), std::chrono::microseconds(0));
+    ASSERT_EQ(player_base.GetSpeed(), 1.0); 
+}
+
+TEST(PlayerBase, SetGetSpeed) { 
+    cxxmidi::guts::PlayerBase player_base;
+    player_base.SetSpeed(2.0);
+    ASSERT_EQ(player_base.GetSpeed(), 2.0); 
+}
